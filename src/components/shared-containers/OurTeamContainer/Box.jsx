3927afa024a224b8c4cd1b21f791729b51f67c11
nishant25062002 <mainwalnishant9@gmail.com>
2023-05-28 21:57:07 +0530

    Push

--- src/components/shared-containers/OurTeamContainer/Box.jsx
+++ src/components/shared-containers/OurTeamContainer/Box.jsx
@@ -4,7 +4,9 @@ import './Box.css'
 const Box = ({data}) => {
     return (
         <div className='box'>
+            <div className='box_img'>
             <img src={data?.img} alt=''/>
+            </div>
            <div className='divBox'> 
             <h4>{data?.name}</h4>
             <p>Senior Accountant</p>
